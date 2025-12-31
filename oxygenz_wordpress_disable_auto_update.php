ca2d56cc8cfe5ac128eae6caab1ed577d75bb248
jladoire <113584160+jladoire@users.noreply.github.com>
2025-12-31 12:09:06 +0100

    Update compatibilité with WordPress 6.9

--- oxygenz_wordpress_disable_auto_update.php
+++ oxygenz_wordpress_disable_auto_update.php
@@ -2,11 +2,25 @@
 /*
 Plugin Name: Oxygenz - Disable all auto-updates
 Description: Disable core, plugins & themes auto-updates
-Version: 1.0
+Version: 1.1
 Author: Oxygenz
 */
 
-add_filter('auto_update_core'		,'__return_false');
-add_filter('auto_update_plugin'		,'__return_false');
-add_filter('auto_update_theme'		,'__return_false');
-add_filter('auto_update_translation','__return_false');
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+add_filter('automatic_updater_disabled', '__return_true');
+
+add_filter('allow_major_auto_core_updates', '__return_false');
+add_filter('allow_minor_auto_core_updates', '__return_false');
+add_filter('allow_dev_auto_core_updates', '__return_false');
+add_filter('auto_update_core', '__return_false');
+
+add_filter('auto_update_plugin', '__return_false');
+add_filter('plugins_auto_update_enabled', '__return_false');
+
+add_filter('auto_update_theme', '__return_false');
+add_filter('themes_auto_update_enabled', '__return_false');
+
+add_filter('auto_update_translation', '__return_false');
